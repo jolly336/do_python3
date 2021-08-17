eba2057ffb9c5736abbccd8cc13e2d74472b4094
wanghaoxun <wanghaoxun@bytedance.com>
2021-08-17 23:46:15 +0800

    update oop

--- samples/oop/student11.py
+++ samples/oop/student11.py
@@ -17,6 +17,8 @@
 # 类是个模块，可以生成各种不同对象
 
 """
+没有什么是不能访问
+
 student10.py 最后思考题：为什么给方法添加双下滑下调用就报错，而实例变量添加调用不报错
 student1.__score = -1
 """
@@ -51,13 +53,16 @@ student1 = Student('Nelson', 18)
 student2 = Student('Jolly', 18)
 
 # 为什么给方法添加双下滑下调用就报错，而实例变量添加调用不报错
-# 由于__score为私有，所以此时是给student1对象新添加了一个实例变量
-student1.__score = -1
-print(student1.__score)
-# python把私有变量名称修改为 _class__var 而阻止外部访问
-print(student1._Student__score)
+# 由于 __score 为私有，所以此时是给 student1 对象新添加了一个实例变量
+student1.__score = -13
+# Python 不能动态添加私有变量的！
+print(student1.__score)  # 注意：Python 动态语言的特性，修改的不是我们实例里面的私有 __score 变量，而是新添加了一个 __score 变量！
+# print(student2.__score) # 报错，AttributeError: 'Student' object has no attribute '__score'
+
 # {'name': 'Nelson', 'age': 18, '_Student__score': 0, '__score': -1}
-print(student1.__dict__)
+print(student1.__dict__)  # 查看实例变量，其中 __Student__score 是 Python 把 __score 更改为这个
+# python 把私有变量名称修改为 _class__var 而阻止外部访问，可以看到 Python 私有机制是比较弱的
+print(student1._Student__score)  # 读取被 Python 更改后的变量，严格来说 Python 是没有私有的。。。
 # AttributeError: 'Student' object has no attribute '__score'
 # print(student2.__score)
 # {'name': 'Jolly', 'age': 18, '_Student__score': 0}
--- samples/oop/student12.py
+++ samples/oop/student12.py
@@ -18,8 +18,21 @@
 
 from human import Human
 
+"""
+继承（Python 可多继承）
 
-class Student(Human):
+三大特性：继承性、封装性、多态性
+
+继承性的作用：
+
+* 避免定义重复的方法和变量
+* 
+
+Python 虽然可以一个模块可以写多个类，但是建议：一个模块只写一个类！
+"""
+
+
+class Student(Human):  # 括号里的是父类
     # 学生数量
     # sum = 0
 
@@ -28,18 +41,19 @@ class Student(Human):
         self.school = school
         # 调用父类构造函数
         # TypeError: __init__() missing 1 required positional argument: 'age'
-        # 为什么要传入self给父类？？-普通方法，通过类调用要传递self，实例方法不需要
+        # 为什么要传入 self 给父类？？-普通方法，通过类调用要传递 self，实例方法不需要
         # Human.__init__(name, age)
-        # 「第一种」调用父类
-        Human.__init__(self, name, age)
+        # 「第一种」调用父类，用类调用实例方法
+        # 不推荐！如果哪天修改了父类，所有使用的地方都要做修改！
+        Human.__init__(self, name, age)  # 直接调用父类构造函数，一个类调用了 init 示例方法，面向对象角度理解是不正确的，
 
-        # 「第二种」调用父类
-        super(Student, self).__init__(name, age)
+        # 「第二种」调用父类-推荐！
+        super(Student, self).__init__(name, age)  #
         # self.name = name
         # self.age = age
 
     def do_homework(self):
-        super(Student, self).do_homework()
+        super(Student, self).do_homework()  # super 还可以使用在方法里
         print('child homework')
 
 
@@ -53,6 +67,7 @@ student1.do_homework()
 
 # TypeError: do_homework() missing 1 required positional argument: 'self'
 # Student.do_homework()
+Student.do_homework(student1)  # what? 可以运行，直接使用类调用方法，需要传入 self 参数。完全多此一举，只为了演示！
 # 随便传入''，不报错，可以执行
 # Student.do_homework('')
 
