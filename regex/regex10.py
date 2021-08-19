b4e2226bef5b088dd588961194ee2854219d8172
wanghaoxun <wanghaoxun@bytedance.com>
2021-08-19 23:52:07 +0800

    update regex

--- regex/regex10.py
+++ regex/regex10.py
@@ -8,11 +8,25 @@
 
 import re
 
+"""
+匹配模式参数
+
+"""
+
 a = 'PythonC#\nJavaPHP'
 
+r = re.findall('c#', a)
+print(r)  # [] 啥也没匹配到，你是小写的 c#
+
+# def findall(pattern, string, flags=0) 最后一个参数加模式
+r = re.findall('c#', a, re.I)  # 忽略大小写
+print(r)  # ['C#']
+
 # re.I 或略字母大小写；re.S 匹配所有字符，包括换行
-r = re.findall('c#.{1}', a, re.I)
-print(r)
+r = re.findall('c#.{1}', a, re.I)  # 要匹配 c# 再加一个任意字符
+print(r)  # []
+
+# 概括字符集 . 表示除 \n 换行符以外的所有字符，加上 re.S 模式之后，就可以匹配所有字符了
+r = re.findall('c#.{1}', a, re.I | re.S)  # 说明：这里 | 不是我们理解的或关系，而是且关系。
 # 且，又要或略大小写，又要忽略对.行为的改变
-r = re.findall('c#', a, re.I | re.S)
-print(r)
+print(r)  # ['C#\n']
--- regex/regex9.py
+++ regex/regex9.py
@@ -8,10 +8,23 @@
 
 import re
 
+"""
+组
+
+(组){次数}(另外一个组)
+
+思考：对比之前 [] 和 () 在逻辑上有啥不一样的？
+
+[abc] 是或的关系
+(abc) 是且的关系
+
+"""
+
+# 问题：判断 a 字符串是否包含 3个 Python？
 a = 'PythonPythonPythonPythonPython'
 
 # [abc] 中间字符是或的关系，() 是且的关系
-r = re.findall('(Python){3}(JS)', a)
+# r = re.findall('PythonPythonPython', a) # 最直接的想法是写三个 Python，emmm 也可以。要判断 100次呢？写一百次？？？
+# r = re.findall('Python{3}', a) # 这样写只是 n 显示 3次！！
+r = re.findall('(Python){3}(JS)', a)  # 组，使用 () 就可以
 print(r)
-
-
