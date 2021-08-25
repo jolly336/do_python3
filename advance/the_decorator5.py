6a66920928c48a2ca2401e98c91c2023270ec5ed
wanghaoxun <wanghaoxun@bytedance.com>
2021-08-25 23:29:28 +0800

    update advance

--- advance/the_decorator5.py
+++ advance/the_decorator5.py
@@ -9,6 +9,11 @@
 """
 装饰器五
 
+进一步优化下装饰器
+
+the_decorator4.py 使用 *args 可变参数可以解决函数拥有不同长度参数列表的问题。
+
+支持：**kw 关键字参数
 """
 
 # 类库，框架里面使用了大量装饰器
@@ -18,11 +23,12 @@
 # 1. 打印函数执行时间
 import time
 
+
 # 1. 可变参数实现
 def decorator(func):
-    def wrapper(*args, **kw):
+    def wrapper(*args, **kw):  # 兼容关键字参数
         print(time.time())
-        func(*args, **kw)
+        func(*args, **kw)  # 在不知道如何传参时，抽象，可以使用 *args **kw
 
     return wrapper
 
@@ -41,6 +47,7 @@ def f2(func_name1, func_name2):
     print('This is a function named ' + func_name1)
     print('This is a function named ' + func_name2)
 
+
 # 关键字参数 key word
 @decorator
 def f3(func_name1, func_name2, **kw):
@@ -48,6 +55,7 @@ def f3(func_name1, func_name2, **kw):
     print('This is a function named ' + func_name2)
     print(kw)
 
+
 f1('test func')
 f2('test func1', 'test func2')
-f3('test func1', 'test func2', a = 1, b = 2, c= '123')
+f3('test func1', 'test func2', a=1, b=2, c='123')
--- advance/the_flask.py
+++ advance/the_flask.py
@@ -7,20 +7,27 @@
 # @Site    : nelsonblog.me
 
 """
-
+flash web 框架
 """
+
 
 # 装饰器
 # 控制器
 
 # 成百上千个接口，在需要的函数上添加`控制器`即可
+# 访问控制器的 url 地址，http 方法
 @api.route('/get', methods=['GET'])
 def test_javascript_http():
     p = request.args.get('name')
     return p, 200
 
+
+# 一个中大型项目接口 三五十个太正常了，rest 接口能少点，如果不是 rest，业务性接口成百上千很正常
+
 # 权限控制，身份验证，需要验证才能调用控制器，这里可以看到装饰器很灵活
-# 了解AOP思想
+# 了解AOP思想，从一个横切面来解决问题
+# 多个装饰器
+# 图书信息服务，任何人都可以访问，像豆瓣 api。但删除接口不是任何人都能访问
 @api.route('/psw', methods=['GET'])
 @auth.login_required
 def get_psw():
