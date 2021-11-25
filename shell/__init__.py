35a018b7f6ba2209e378902115221f9314d1c990
wanghaoxun <wanghaoxun@bytedance.com>
2021-11-25 11:29:44 +0800

    added shell dir

---
+++ shell/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# @Desc    :
+# @Time    : 2021/11/25 11:22 上午
+# @Author  : Nelson
+# @Contact : haoxunwang525@gmail.com
+# @Site    : nelsonblog.me
---
+++ shell/hello.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "hello shell ${1} ${2}"
+exit 0
